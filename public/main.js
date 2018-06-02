5128d74d82f858d6f071efca6a4329e2adc5fb2d
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-03 00:18:31 +0530

    table added

--- public/main.js
+++ public/main.js
@@ -1,9 +1,18 @@
 let socket  =  io.connect('http://localhost:4000');
 
 var time = document.getElementById('time');
+var table = document.getElementById('table');
 
+for(var i=0;i<10;i++){
+    var row = table.insertRow(i);  
+    var id = row.insertCell(0);
+    var color = row.insertCell(1);
+}
 //Listen for the emits
 socket.on('update',(data)=>{
-    var json_data = JSON.stringify(data);
-    console.log(json_data);
+    data.map((i,key)=>{
+        console.log(table.rows[key].getElementsByTagName("td"));
+        table.rows[key].getElementsByTagName("td")[0].innerHTML = i.equip_id;
+        table.rows[key].getElementsByTagName("td")[1].innerHTML = i.color;
+    })  
 });
