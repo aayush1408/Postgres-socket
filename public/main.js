f008093e328c64ee28d14b422b1bc77ff3773397
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-02 13:05:36 +0530

    displayed the latest 10 result in console

--- public/main.js
+++ public/main.js
@@ -4,5 +4,6 @@ var time = document.getElementById('time');
 
 //Listen for the emits
 socket.on('update',(data)=>{
-    time.innerHTML = data;
+    var json_data = JSON.stringify(data);
+    console.log(json_data);
 });
