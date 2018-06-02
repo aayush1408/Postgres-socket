67af3cbe5e3ca99a90e6461e8d2113255cb30339
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-02 13:04:37 +0530

    Connected and written the query

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const socket = require('socket.io');
-
 const app = express();
+const { Client } = require('pg')
+var conString = "postgres://test:test@localhost:5432/test";
+
+var client = new Client(conString);
+
+// client.connect();
+// client.connect().then(()=>{
+//   console.log('Connected');
+//   const sql = 'INSERT INTO test (color) VALUES ($1)';
+//   const params = ['orange'];
+//   return client.query(sql,params);
+// }).then((result)=>{
+//   console.log(result);
+// }).
+// catch(e => console.error('connection error', e.stack))
+
+
 app.use(express.static('public'));
 const server = app.listen('4000',()=>{
     console.log('Server running at 4000');
@@ -10,9 +26,15 @@ const server = app.listen('4000',()=>{
 const io = socket(server);
 
 io.on('connection',(socket)=>{
+  client.connect().then(()=>{
+    console.log('Connected');
+  });
   setInterval(()=>{
-  const date = new Date();      
-  socket.emit('update',date);      
-  console.log(date);
+  client.query('SELECT * FROM test ORDER BY equip_id DESC LIMIT 10').then((result)=>{
+    socket.emit('update',result.rows);          
+    console.log(result.rows);
+  }).catch((err)=>{
+    console.log(err);
+  });
   },1000);  
-})
+});
